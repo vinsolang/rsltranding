f315ddfdda39e44cc2f34cec263e76bae6007aa2
vinsolang <vinsolang9@gmail.com>
2026-01-28 14:37:08 +0700

    update

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1044,8 +1044,8 @@
             <img src="{{ asset('assets/images/bg-1.png') }}" class="-z-40">
         </div>
         <div class="md:mt-1 m-4kk">
-            <div class="relative mx-auto max-w-6xl px-4 md:px-80 md:top-0 top-32">
-                <div class="gap-b-32">
+            <div class="relative mx-auto max-w-6xl px-4 md:px-80 md:top-0 top-36">
+                <div class="gap-b-32 md:space-y-0 space-y-6">
                     {{-- Vision --}}
                     <div class="relative mx-auto min-h-14 md:mb-12 max-w-4xl">
                         {{-- Background image (BEHIND text) --}}
