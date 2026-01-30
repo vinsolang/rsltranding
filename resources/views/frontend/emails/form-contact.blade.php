6dd5035f6e6d6d57dcb449f0c12026e32cf540b8
vinsolang <vinsolang9@gmail.com>
2026-01-30 16:14:43 +0700

    update

--- resources/views/frontend/emails/form-contact.blade.php
+++ resources/views/frontend/emails/form-contact.blade.php
@@ -7,20 +7,20 @@
 
 @if(!empty($data['cv_path']))
     <p>
-        <strong>CV:</strong>
-        <a href="{{ Storage::url($data['cv_path']) }}" target="_blank">
+        <strong>CV and Cover Letter</strong>
+        {{-- <a href="{{ Storage::url($data['cv_path']) }}" target="_blank">
             View / Download CV
-        </a>
+        </a> --}}
         {{-- <img src="{{ asset('storage/' . $data['cv_path']) }}" alt=""> --}}
     </p>
 @endif
 
-@if(!empty($data['cover_letter_path']))
+{{-- @if(!empty($data['cover_letter_path']))
     <p>
         <strong>Cover Letter:</strong>
         <a href="{{ Storage::url($data['cover_letter_path']) }}" target="_blank">
             View / Download Cover Letter
         </a>
-        {{-- <img src="{{ asset('storage/' . $data['cover_letter_path']) }}" alt=""> --}}
+       
     </p>
-@endif
+@endif --}}
